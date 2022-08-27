47a48359470bda3dcaaabf709a0c5bd9a44c8312
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-08-27 22:19:50 +0300

    Update server_examples.py
    Final update to server

--- server_examples.py
+++ server_examples.py
@@ -1,40 +1,49 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+from numpy import asarray
+from knnserver import Knn
+import cv2
 
 app = Flask(__name__)
+
+# Init the mnist library
+mnist = load_digits()
+X = mnist.data
+y = mnist.target
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return 'This is home page'
 
 
 @app.route("/upload", methods=['POST'])
 def handle_file_upload():
-    try:
-        photo = request.files['images']
-    except FileNotFoundError:
-        return 'failed to upload image'
+    if 'image' in request.files:
+        # In case a file was sent
+        photo = request.files['image']
+        # If the file is valid
+        if photo.filename != '':
+            # Save the file to folder
+            photo.save(os.path.join('.', photo.filename))
+            # Read the file to grayscale picture
+            picture = cv2.imread('model.png', 0)
+            # Convert the picture to numpy array
+            np_picture = asarray(picture)
+            # Reshape in favor of the knn predictore
+            photo_np = np_picture.reshape(64)
+            # init the knn class
+            knn_cls = Knn(k=3)
+            knn_cls.fit()
+            # make a prediction
+            prediction = knn_cls.predict(photo_np)
+            return str(prediction)
     else:
-        photo.save(os.path.join('.', photo.filename))
-        print("image saved")
-
-    class KNN:
-        def __init__(self):
-            self.number = "Its a one"
-
-        def find_number(self):
-            pass
-
-    knn_clf = KNN()
-
-    return knn_clf.number
-
-
-@app.route("/choose_picture", methods=['GET'])
-def choose_mnist_picture():
-    return 'static/Dog.jpg'
+        # No image was sent or bad connection issue
+        return "Fail"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
