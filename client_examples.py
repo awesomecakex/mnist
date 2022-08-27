eeef167ccfab7b27db9ffb8e7308c00996ac1056
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-08-27 22:20:15 +0300

    Update client_examples.py
    Last update to client

--- client_examples.py
+++ client_examples.py
@@ -1,12 +1,28 @@
 import requests as req
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+import cv2
+
+# Init the mnist library
+mnist = load_digits()
+X = mnist.data
+y = mnist.target
 
 url_to_upload = "http://127.0.0.1:5000/upload"
-url_random_picture = "http://127.0.0.1:5000/choose_picture"
 
-picture = req.get(url_random_picture)
-
-
-with open(picture.text, 'rb') as f:
-    files = {'images': f}
+# Getting the number from the user
+choice = int(input("Enter number between 0-1947: "))
+picture = mnist.images[choice]
+print("You chose the following number")
+plt.imshow(picture, cmap='gray')
+plt.show()
+# Saving picture to file
+plt.imsave('images/model.png', picture)
+# Sending picture to server
+with open('images/model.png', 'rb') as f:
+    files = {'image': f}
     r = req.post(url_to_upload, files=files)
-    print(r.text)
+    if r.status_code == 200 and r.status_code != 'Fail':
+        print(f"The classify predict {r.text}")
+    else:
+        print("Something went very wrong!!!")
